c96d5412be5079939b70745dd1a6874f834962f1
Rosario Garufi <saro.garufi1@gmail.com>
2022-05-09 15:26:22 +0200

    bug

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <!-- searchbar -->
     <header>
-      <form id="form">
-        <input type="text" placeholder="Search..." id="search">
-        <button id="search">search</button>
-      </form>
+      
+        <input @keyup.enter="searchbox" v-model="searchbox" type="text" placeholder="Search..." id="search">
+        <button @click="filterMovie" id="search">search</button>
+      
     </header>
     <!-- visualizzare: titolo, titolo originale, lingua, voto -->
   
@@ -38,6 +38,7 @@ export default {
   data() {
 
     return {
+      searchbox: "",
       URL_APi: "https://api.themoviedb.org/3/search/movie?api_key=2d4086a1da1ceb84b071c2d1750dc6c4&language=it-IT&page=1&include_adult=false&query=string",
       movies: [],
 
@@ -62,11 +63,22 @@ export default {
           error;
           this.error = "Ci sono dei problemi...";
         });
+    },
+
+    filterMovie(){
+      const movie_filtr = [];
+      if(this.movies.includes(this.searchbox)){
+        console.log("ok");
+        movie_filtr.push(this.movies);
+        console.log(movie_filtr);
+      }
+    }
+
+    
 
         
 
 
-    }
   },
   mounted() {
     this.callApi();
