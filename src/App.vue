a12a9163567c04e68712d22ce745d0d9eba5da9c
Rosario Garufi <saro.garufi1@gmail.com>
2022-05-09 14:55:19 +0200

    print info array

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
     <!-- searchbar -->
-   <header>
-     <form id="form">
-       <input type="text" placeholder="Search..." id="search">
-       <button id="search">search</button>
-     </form>
-   </header>
-    
+    <header>
+      <form id="form">
+        <input type="text" placeholder="Search..." id="search">
+        <button id="search">search</button>
+      </form>
+    </header>
+    <!-- visualizzare: titolo, titolo originale, lingua, voto -->
+  
+    <main>
+      <ul>
+        <li v-for="movie in movies" :key="movie.id">
+          
+          <h3>{{movie.title}}</h3>
+          <h2>{{movie.original_title}}</h2>
+          <h2>{{movie.original_language}}</h2>
+          <h2>{{movie.vote_average}}</h2>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
@@ -16,70 +28,80 @@
 <script>
 import axios from "axios";
 export default {
+
   name: 'App',
+
   components: {
-    
+
   },
 
- data(){
-   return{
-     URL_APi: "https://api.themoviedb.org/3/search/movie?api_key=2d4086a1da1ceb84b071c2d1750dc6c4&language=it-IT&page=1&include_adult=false&query=string",
-     movie: [],
-     
-     error: null, 
-   };
- },
+  data() {
 
- methods: {
-   callApi(){
-    axios
-    .get(this.URL_APi)
-    .then((response)=>{
-      console.log(response);
-      this.movie = response.data.results;
-      
-      console.log(response.data.results);
-      
-      
-      
-    })
-    .catch((error)=>{
-      
-      error;
-      this.error= "Ci sono dei problemi...";
-    });
+    return {
+      URL_APi: "https://api.themoviedb.org/3/search/movie?api_key=2d4086a1da1ceb84b071c2d1750dc6c4&language=it-IT&page=1&include_adult=false&query=string",
+      movies: [],
 
-   }
- },
- mounted(){
-   this.callApi();
- }
-}
+      error: null,
+    };
+
+  },
+
+  methods: {
+    callApi() {
+      axios
+        .get(this.URL_APi)
+        .then((response) => {
+
+          this.movies = response.data.results;
+          console.log(this.movies);
+
+        })
+
+        .catch((error) => {
+          console.log(error)
+          error;
+          this.error = "Ci sono dei problemi...";
+        });
+
+        
+
+
+    }
+  },
+  mounted() {
+    this.callApi();
+  },
+};
+
+
+
+
+
+
 </script>
+
+
+
 
 <!-- api: https://api.themoviedb.org/3/search/movie?api_key=2d4086a1da1ceb84b071c2d1750dc6c4&language=it-IT&page=1&include_adult=false&query=string -->
 
 
 <style lang="scss" scoped>
-*{
-  margin:0;
-  padding:0;
+* {
+  margin: 0;
+  padding: 0;
   box-sizing: border-box;
 }
-header{
+
+header {
   height: 5vh;
-  display:Flex;
+  display: Flex;
   align-items: center;
-  
-  } 
-  
-  #search{
-    padding:5px 10px;
-    margin:10px;
-  }
 
-   
+}
 
-
-
+#search {
+  padding: 5px 10px;
+  margin: 10px;
+}
 </style>
